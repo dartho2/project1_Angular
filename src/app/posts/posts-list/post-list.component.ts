594cbf17ea060575d2c9399cf13754853458a736
Jan Nowak <jannowak@example.com>
2018-08-17 16:36:35 +0200

    subject

--- src/app/posts/posts-list/post-list.component.ts
+++ src/app/posts/posts-list/post-list.component.ts
@@ -1,16 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
     selector: 'app-post-list',
     templateUrl: './post-list.component.html',
     styleUrls:  ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
     // posts =[
     //     {title: 'First Post', content: 'asda'},
     //     {title: 'First a', content: 'asda'},
     //     {title: 'First c', content: 'asda'}
     // ]
-@Input() posts =[]
+    posts: Post[] = []
+    private postsSub: Subscription;
 
+    constructor(public postsService: PostsService) {}
+
+    ngOnInit() {
+        this.posts = this.postsService.getPosts();
+        this.postsSub = this.postsService.getPostUpdatedListener()
+        .subscribe((posts: Post[]) => {
+            this.posts = posts;
+        }
+
+        );
+        
+    }
+    ngOnDestroy() {
+        this.postsSub.unsubscribe();
+    }
 }
